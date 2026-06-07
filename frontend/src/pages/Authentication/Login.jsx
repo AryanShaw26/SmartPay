09b5f04deaf5c9657f458e6e5769635286374c61
unknown <aryanshaw344@gmail.com>
2026-06-07 09:50:11 +0530

    Added useState in Login and Register page

--- frontend/src/pages/Authentication/Login.jsx
+++ frontend/src/pages/Authentication/Login.jsx
@@ -2,87 +2,72 @@ import "./Login.css";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
 function Login() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email: ", email);
+    console.log("Password: ", password);
+    navigate("/dashboard");
+  };
   return (
     <div className="login-container">
-
       <div className="login-card">
-
         <div className="login-header">
-
           <h1>
             Smart<span>Pay</span>
           </h1>
 
           <h2>Welcome Back</h2>
 
-          <p>
-            Login to access your wallet and transactions.
-          </p>
-
+          <p>Login to access your wallet and transactions.</p>
         </div>
 
-        <form className="login-form">
-
+        <form className="login-form" onSubmit={handleSubmit}>
           <div className="input-group">
-
             <FaEnvelope className="input-icon" />
 
             <input
               type="email"
               placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-
           </div>
 
           <div className="input-group">
-
             <FaLock className="input-icon" />
 
             <input
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-
           </div>
 
           <div className="remember-forgot">
-
             <label>
               <input type="checkbox" />
               Remember Me
             </label>
 
-            <a href="/forgot-password">
-              Forgot Password?
-            </a>
-
+            <a href="/forgot-password">Forgot Password?</a>
           </div>
 
-          <button
-            type="submit"
-            className="login-btn"
-            onClick={()=>navigate("/dashboard")}
-          >
+          <button type="submit" className="login-btn">
             Login
           </button>
-
         </form>
 
         <div className="login-footer">
-
           Don't have an account?
-
-          <Link to ="/register">
-            Register
-          </Link>
-
+          <Link to="/register">Register</Link>
         </div>
-
       </div>
-
     </div>
   );
 }
--- frontend/src/pages/Authentication/Register.jsx
+++ frontend/src/pages/Authentication/Register.jsx
@@ -1,106 +1,103 @@
 import "./Register.css";
 import { FaUser, FaEnvelope, FaPhone, FaLock } from "react-icons/fa";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 function Register() {
+  const navigate = useNavigate();
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("FullName:", fullName);
+    console.log("Email: ", email);
+    console.log("Phone Number: ", phoneNumber);
+    console.log("Password: ", password);
+    console.log("Confirm Password: ", confirmPassword);
+
+    navigate("/dashboard");
+  };
   return (
     <div className="register-container">
-
       <div className="register-card">
-
         <div className="register-header">
-
           <h1>
             Smart<span>Pay</span>
           </h1>
 
           <h2>Create Account</h2>
 
-          <p>
-            Join SmartPay and experience secure digital payments.
-          </p>
-
+          <p>Join SmartPay and experience secure digital payments.</p>
         </div>
 
-        <form className="register-form">
-
+        <form className="register-form" onSubmit={handleSubmit}>
           <div className="input-group">
-
             <FaUser className="input-icon" />
 
             <input
               type="text"
               placeholder="Full Name"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
             />
-
           </div>
 
           <div className="input-group">
-
             <FaEnvelope className="input-icon" />
 
             <input
               type="email"
               placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-
           </div>
 
           <div className="input-group">
-
             <FaPhone className="input-icon" />
 
             <input
               type="tel"
               placeholder="Phone Number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
             />
-
           </div>
 
           <div className="input-group">
-
             <FaLock className="input-icon" />
 
             <input
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-
           </div>
 
           <div className="input-group">
-
             <FaLock className="input-icon" />
 
             <input
               type="password"
               placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
-
           </div>
 
-          <button
-            type="submit"
-            className="register-btn"
-            onClick={()=>navigate("/dashboard")}
-          >
+          <button type="submit" className="register-btn">
             Create Account
           </button>
-
         </form>
 
         <div className="register-footer">
-
           Already have an account?
-
-          <Link to="/login">
-            Login
-          </Link>
-
+          <Link to="/login">Login</Link>
         </div>
-
       </div>
-
     </div>
   );
 }
