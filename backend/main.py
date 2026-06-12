27197ad8f335395769f034e81dcbb37d4a8e4aef
unknown <aryanshaw344@gmail.com>
2026-06-12 16:44:31 +0530

    Fix Vercel frontend URL in CORS

--- backend/main.py
+++ backend/main.py
@@ -38,9 +38,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-
-        # future frontend deployment
-        "https://smart-ad21ctzhk-aryan-shaw-s-projects.vercel.app",
+        "https://smart-pay-dusky.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
