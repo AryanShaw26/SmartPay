0567a188a0cc1264ca7f0dcde4701e842ab2e677
unknown <aryanshaw344@gmail.com>
2026-06-12 15:56:14 +0530

    vercel-code-enhanced

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
 
         # future frontend deployment
-        "https://smartpay.vercel.app",
+        "https://smart-pay-ten.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
